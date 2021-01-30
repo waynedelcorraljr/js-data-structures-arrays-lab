7aecfa88a3d3ae0e9b0fadaef7e97f91cf889d7f
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-01-29 22:58:18 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    return drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    return drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+    return drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+    return drivers.shift();
+}
+
+function appendDriver(name) {
+    return [...drivers, name]
+}
+
+function prependDriver(name) {
+    return [name, ...drivers]
+}
+
+function removeLastDriver() {
+    let driverGettinPopped = [...drivers]
+    driverGettinPopped.pop()
+    return driverGettinPopped;
+}
+
+function removeFirstDriver() {
+    let driverGettinPopped = [...drivers]
+    driverGettinPopped.shift()
+    return driverGettinPopped;
+}
+    
